81826b98f2266d1ec1d116fdf1057944d1a7f94f
tannguyen51 <tannxse181599@fpt.edu.vn>
2025-05-13 22:40:35 +0700

    a

---
+++ BTVN01.1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BTVN01._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a + " " + b);
+
+            static void HoanDoi(ref int a, ref int b)
+            {
+                int hd = a;
+                a = b;
+                b = hd;
+            }
+
+            HoanDoi(ref a, ref b);
+
+            Console.WriteLine(a + " " + b);
+
+
+        }
+    }
+}
