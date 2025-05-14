a81b10b01eba41d41bc0f5fab1bab7d8e21c163f
tannguyen51 <tannxse181599@fpt.edu.vn>
2025-05-14 16:29:45 +0700

    a

---
+++ BTVN2.2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BTVN2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SinhViencs[] ds = new SinhViencs[3];
+            for (int i = 0; i < ds.Length; i++)
+            {
+                Console.WriteLine($"Nhâp thong tin sinh vien thu {i + 1}");
+                ds[i] = new SinhViencs();
+                ds[i].NhapThongTin();
+
+            }
+
+            foreach (var sv in ds)
+            {
+                sv.HienThiThongTin();
+            }
+        }
+    }
+}
---
+++ BTVN2.2/SinhViencs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTVN2._2
+{
+    internal class SinhViencs
+    {
+        public string MaSV { get; set; }
+        public string TenSV { get; set; }
+        public double DiemTB { get; set; }
+
+        public void NhapThongTin()
+        {
+            Console.WriteLine("Mã SInh Viên :" );
+            MaSV = Console.ReadLine();
+            Console.WriteLine("Ten Sinh Vien:");
+            TenSV = Console.ReadLine();
+            Console.WriteLine("DIem TB:" );
+            DiemTB = double.Parse(Console.ReadLine());
+        }
+        public void HienThiThongTin()
+        {
+            Console.WriteLine($"Ma SV : {MaSV}, Ten Sinh Vien : {TenSV}, Diem Trung Binh : {DiemTB}, Xep Loai : {XepLoai()}");
+        }
+        public String XepLoai()
+        {
+            if (DiemTB >= 8)
+            {
+                return "Gioi";
+            }
+            else if (DiemTB >= 5)
+            {
+                return "Kha";
+            }
+            else
+            {
+                return "Trung Binh";
+
+            }
+        }
+    }   
+}
