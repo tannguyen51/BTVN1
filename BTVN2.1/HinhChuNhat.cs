e5d21a4ca2dd48acb35d11a54c3baea3d20dc1f2
tannguyen51 <tannxse181599@fpt.edu.vn>
2025-05-14 15:54:25 +0700

    a

---
+++ BTVN2.1/HinhChuNhat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTVN2._1
+{
+    internal class HinhChuNhat
+    {
+        
+        public  double chieuDai { get; set; }
+        public double chieuRong { get; set; }
+        public double TinhChuVi()
+        {
+            return (chieuDai + chieuRong) * 2;
+        }
+        public double TinhDienTich()
+        {
+            return chieuDai * chieuRong;
+        }
+        public void HienThiThongTin()
+        {
+            Console.WriteLine("Chieu dai la " + chieuDai);
+            Console.WriteLine("Chieu rong la " + chieuRong);
+            Console.WriteLine("Chu Vi la " + TinhChuVi());
+            Console.WriteLine("Dien tich la " + TinhDienTich());
+
+        }
+    }
+}
---
+++ BTVN2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BTVN2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HinhChuNhat hcn = new HinhChuNhat();
+             hcn.chieuDai = 5.5;
+            hcn.chieuRong = 6.2;
+            hcn.HienThiThongTin();
+
+        }
+    }
+}
